ee61d349053bddadca600888620851efd6190b16
Amber Iqbal <amber.dev.02@gmail.com>
2024-09-07 01:37:48 -0700

    Added some functional features

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import "./App.css";
 import Heading from "./components/Heading";
 import Outputscreen from "./components/Outputscreen";
 import Buttons from "./components/Buttons";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const handleButtonClick = (value) => {
+    setInput(input + value);
+  };
+  const handleClear = () => {
+    setInput("");
+  };
+
   return (
     <>
       <Heading />
       <Outputscreen />
-      <Buttons />
+      <Buttons
+        handleButtonClick={handleButtonClick}
+        handleClear={handleClear}
+      />
     </>
   );
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,5 +1,5 @@
 import React from "react";
-function Buttons() {
+function Buttons(props) {
   const buttonContainerStyle = {
     display: "grid",
     gridTemplateColumns: "repeat(6, 100px)",
@@ -19,28 +19,139 @@ function Buttons() {
   return (
     <div style={buttonContainerStyle}>
       {/*first row */}
-      <button style={buttonStyle}>Clear</button>
-      <button style={buttonStyle}>Delete</button>
-      <button style={buttonStyle}>.</button>
-      <button style={buttonStyle}>/</button>
+      <button style={buttonStyle} onClick={props.handleClear}>
+        Clear
+      </button>
+      <button style={buttonStyle} onClick={props.handleDelete}>
+        Delete
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick(".");
+        }}
+      >
+        .
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("/");
+        }}
+      >
+        /
+      </button>
       {/*2nd row */}
-      <button style={buttonStyle}>7</button>
-      <button style={buttonStyle}>8</button>
-      <button style={buttonStyle}>9</button>
-      <button style={buttonStyle}>*</button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("7");
+        }}
+      >
+        7
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("8");
+        }}
+      >
+        8
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("9");
+        }}
+      >
+        9
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("*");
+        }}
+      >
+        *
+      </button>
       {/*3rd row */}
-      <button style={buttonStyle}>4</button>
-      <button style={buttonStyle}>5</button>
-      <button style={buttonStyle}>6</button>
-      <button style={buttonStyle}>-</button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("4");
+        }}
+      >
+        4
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("5");
+        }}
+      >
+        5
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("6");
+        }}
+      >
+        6
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("-");
+        }}
+      >
+        -
+      </button>
       {/*4th row */}
-      <button style={buttonStyle}>1</button>
-      <button style={buttonStyle}>2</button>
-      <button style={buttonStyle}>3</button>
-      <button style={buttonStyle}>+</button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("1");
+        }}
+      >
+        1
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("2");
+        }}
+      >
+        2
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("3");
+        }}
+      >
+        3
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("+");
+        }}
+      >
+        +
+      </button>
       {/*fifth row */}
-      <button style={buttonStyle}>0</button>
-      <button style={buttonStyle}>=</button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          props.handleButtonClick("0");
+        }}
+      >
+        0
+      </button>
+      <button style={buttonStyle} onClick={props.handleCalculate}>
+        =
+      </button>
     </div>
   );
 }
